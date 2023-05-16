441813fe94f2f35f1239635dc55bfb0fb2d9b2cb
brianchemase <brianchemo@gmail.com>
2023-05-16 16:01:42 +0300

    upload sql task

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -2,7 +2,7 @@
 -- New row:
 
 --    id = 89
---    name = Holberton School
+--    name = Best School
 -- The database name will be passed as an argument of the mysql command
 
-INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Holberton School')
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School')
