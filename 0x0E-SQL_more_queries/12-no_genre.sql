b4438d86af55cf6e0241de4b9c8696ee13da3a7a
brianchemase <brianchemo@gmail.com>
2023-05-17 20:34:53 +0300

    upload task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,13 @@
-SELECT tv_shows.title AS title, tv_show_genres.genre_id
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+-- Each record should display: tv_geners.name - number of shows
+-- Don't display a genere that doesn't have any shows linked
+-- Results must be sorted in descending order by the number of shows linked 
+-- You can use only SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
-ORDER BY title ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
