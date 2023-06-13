9e62855efc3b975d448ab6766e501dd9bf519d95
brianchemase <brianchemo@gmail.com>
2023-06-13 19:50:54 +0300

    upload task 5 update

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
+
 const arg = process.argv[2];
-if (isNaN(arg)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + arg);
-}
+
+const convertedNumber = parseInt(arg);
+
+console.log(isNaN(convertedNumber) ? 'Not a number' : 'My number: ' + convertedNumber);
