0c051ee34ca60f63512b0a704a0e5f50d175d5b3
brianchemase <brianchemo@gmail.com>
2023-06-13 19:47:22 +0300

    added task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const arg in args) {
- // console.log(args[arg]);
-  console.log(arg);
+  console.log(args[arg]);
 }
